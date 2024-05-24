3df864f084e5825566136dc27296542f8eb01990
arrase21 <arrase21@gmail.com>
2024-05-24 17:36:20 -0400

    upgrade

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from configuracion.Config import Config
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
-from models.Models import db, initialize_roles
 
-from controllers.Login import logging_bp, login_manager
+from configuracion.Config import Config
+
+# from controllers.Login import logging_bp, login_manager
 from controllers.Cliente import clientes_bp
-from controllers.Valoracion import valoraciones_bp
 from controllers.Pliegues import pliegues_bp
-from controllers.Volumetria import volumetria_bp
 from controllers.Upload import image_bp
+from controllers.Valoracion import valoraciones_bp
+from controllers.Volumetria import volumetria_bp
+from models.Models import db, initialize_roles
 
 # Crear la aplicación Flask
 app = Flask(__name__)
@@ -18,12 +19,12 @@ CORS(app)
 app.secret_key = "tu_clave_secreta"
 db.init_app(app)
 
-login_manager.init_app(app)
+# login_manager.init_app(app)
 
 migrate = Migrate(app, db)
 
 app.register_blueprint(clientes_bp)
-app.register_blueprint(logging_bp)
+# app.register_blueprint(logging_bp)
 app.register_blueprint(valoraciones_bp)
 app.register_blueprint(pliegues_bp)
 app.register_blueprint(volumetria_bp)
