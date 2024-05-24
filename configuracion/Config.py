f63a351ebe46596e56db90ea0cf693cd1071edd0
arrase21 <arrase21@gmail.com>
2024-05-24 17:30:04 -0400

    add Render

--- configuracion/Config.py
+++ configuracion/Config.py
@@ -3,6 +3,8 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URI", "postgresql://postgres:211221@localhost/sena_p"
+        # "DATABASE_URI", "postgresql://postgres:211221@localhost/sena_p"
+        "DATABASE_URI",
+        "postgres://arrase:Spq1Fj4lOWOEaXeOxTIIQ5Yh7DYRaC8u@dpg-cp8g6h8l6cac73c4ilu0-a.oregon-postgres.render.com/sena_p",
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
